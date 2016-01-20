627294b2d96ffd928e065783f6e63a39173a79b5
liberman-anton <antonliberman@gmail.com>
2016-01-20 16:01:55 +0500

    implements Map

---
+++ src/MyHashMap.java
@@ -0,0 +1,68 @@
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by 123 on 20.01.2016.
+ */
+public class MyHashMap implements Map {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        return null;
+    }
+}
